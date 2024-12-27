81286572cc6e6e6188ac273c75906a3355169e69
denisgaldin <denisgaldin@mail.ru>
2024-12-28 00:37:38 +0300

    New commit

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+context = os.getenv('context', 'bstack')
+bstack_userName = os.getenv('bstack_userName', 'dannydaytona_Gpy67S')
+bstack_accessKey = os.getenv('bstack_accessKey', 'ujPu5A4L8vWrMqAqx2Qk')
